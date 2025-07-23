c2a279e031913994ff29d3ac8cf1b417af9ac6d9
mustafa-colakoglu <mustafacolakoglu94@gmail.com>
2025-07-23 22:58:39 +0300

    repository interface updated

--- interfaces/repository.interface.ts
+++ interfaces/repository.interface.ts
@@ -1,23 +1,13 @@
-// Standard timestamp fields for entities
-export interface TimestampFields {
-	id: string;
-	createdAt: Date;
-	updatedAt: Date;
-}
-
 // Generic repository interface for data access layer
 export interface IRepository<IdentifierType, ModelType> {
-	findAll(): Promise<(ModelType & TimestampFields)[]>;
-	findById(id: IdentifierType): Promise<(ModelType & TimestampFields) | null>;
-	create(item: ModelType): Promise<ModelType & TimestampFields>;
-	update(
-		id: IdentifierType,
-		item: Partial<ModelType>
-	): Promise<ModelType & TimestampFields>;
+	findAll(): Promise<ModelType[]>;
+	findById(id: IdentifierType): Promise<ModelType | null>;
+	create(item: ModelType): Promise<ModelType>;
+	update(id: IdentifierType, item: Partial<ModelType>): Promise<ModelType>;
 	delete(id: IdentifierType): Promise<void>;
 	findPaginated(
 		query: Partial<ModelType>,
 		page: number,
 		limit: number
-	): Promise<(ModelType & TimestampFields)[]>;
+	): Promise<ModelType[]>;
 }
