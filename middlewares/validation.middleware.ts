354579c4cd41481e4c93d02c4dface9deda81246
Mustafa Çolakoğlu <mustafacolakoglu94@gmail.com>
2025-12-25 15:29:43 +0000

    assign to transformed

--- middlewares/validation.middleware.ts
+++ middlewares/validation.middleware.ts
@@ -55,7 +55,7 @@ export default function validationMiddleware(
         });
         return; // <-- explicit return
       }
-
+      (req as any)[type] = instance;
       next();
       return; // <-- explicit return
     } catch (err: any) {
