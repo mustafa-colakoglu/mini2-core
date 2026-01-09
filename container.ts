582898e29e866e940e4714ac0d2e9638e0e46613
mustafa-colakoglu <mustafacolakoglu94@gmail.com>
2026-01-09 23:55:08 +0300

    added priority to auto load

--- container.ts
+++ container.ts
@@ -11,6 +11,7 @@ export type DiscoveredBinding = {
 	token: ServiceIdentifier<any>;
 	target: new (...args: any[]) => any;
 	scope: Scope;
+	priority: number;
 };
 
 const DISCOVERY_KEY = Symbol.for('DI_DISCOVERY_REGISTRY');
@@ -23,19 +24,19 @@ function getRegistry(): DiscoveredBinding[] {
 
 export function AutoBind(
 	token: DiscoveredBinding['token'],
-	opts?: { scope?: Scope }
+	opts?: { scope?: Scope; priority?: number }
 ) {
 	const scope = opts?.scope ?? 'Singleton';
 
 	return function (target: any) {
 		const reg = getRegistry();
-		reg.push({ token, target, scope });
+		reg.push({ token, target, scope, priority: opts?.priority ?? 0 });
 	};
 }
 
 export const bindDiscovered = () => {
 	const reg = getRegistry();
-	for (const b of reg) {
+	for (const b of reg.sort((a, b) => a.priority - b.priority)) {
 		if (container.isBound(b.token) && b.scope === 'Singleton') continue;
 
 		const binding = container.bind(b.token).to(b.target);
